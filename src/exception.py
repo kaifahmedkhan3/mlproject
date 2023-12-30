3fe627820bfaa0840584528775afeb01d43bda67
KAIF AHMED KHAN <kaifahmedkhan3@gmail.com>
2023-12-30 20:08:08 +0530

    logging & exception

--- src/exception.py
+++ src/exception.py
@@ -1,5 +1,5 @@
 import sys
-from src.mlproject.logger import logging
+import logging
 
 def error_message_detail(error,error_detail:sys):
     _,_,exc_tb=error_detail.exc_info()
@@ -9,11 +9,13 @@ def error_message_detail(error,error_detail:sys):
 
     return error_message
 
+    
 
 class CustomException(Exception):
-    def __init__(self,error_message,error_details:sys):
+    def __init__(self,error_message,error_detail:sys):
         super().__init__(error_message)
-        self.error_message=error_message_detail(error_message,error_details)
-
+        self.error_message=error_message_detail(error_message,error_detail=error_detail)
+    
     def __str__(self):
         return self.error_message
+
