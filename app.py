ce0ffa7ec300e428adfdcb30d20e67874bfa7b2f
KAIF AHMED KHAN <kaifahmedkhan3@gmail.com>
2024-01-13 16:28:25 +0530

    Prediction pipeline

--- app.py
+++ app.py
@@ -1,19 +1,45 @@
-from src.logger import logging
-from src.exception import CustomException
-from src.components.data_ingestion import DataIngestion
-from src.components.data_ingestion import initiate_data_ingestion
-from src.components.data_ingestion import DataIngestionConfig
-import sys
+from flask import Flask,request,render_template
+import numpy as np
+import pandas as pd
 
+from sklearn.preprocessing import StandardScaler
+from src.pipeline.predict_pipeline import CustomData,PredictPipeline
+
+application=Flask(__name__)
+
+app=application
+
+## Route for a home page
+
+@app.route('/')
+def index():
+    return render_template('index.html') 
+
+@app.route('/predictdata',methods=['GET','POST'])
+def predict_datapoint():
+    if request.method=='GET':
+        return render_template('home.html')
+    else:
+        data=CustomData(
+            gender=request.form.get('gender'),
+            race_ethnicity=request.form.get('ethnicity'),
+            parental_level_of_education=request.form.get('parental_level_of_education'),
+            lunch=request.form.get('lunch'),
+            test_preparation_course=request.form.get('test_preparation_course'),
+            reading_score=float(request.form.get('writing_score')),
+            writing_score=float(request.form.get('reading_score'))
+
+        )
+        pred_df=data.get_data_as_data_frame()
+        print(pred_df)
+        
+
+        predict_pipeline=PredictPipeline()
+        
+        results=predict_pipeline.predict(pred_df)
+        
+        return render_template('home.html',results=results[0])
+    
 
 if __name__=="__main__":
-    logging.info("The execution has started")
-
-    try:
-        data_ingestion=DataIngestionConfig()
-        data_ingestion=DataIngestion()
-        data_ingestion=initiate_data_ingestion(data_ingestion)
-        
-    except Exception as e:
-        logging.info("Custom Exception")
-        raise CustomException(e,sys)
+    app.run(host="0.0.0.0",debug=True)        
